bb74dcf7c79290df7aedc945e5b83c0df508708c
Saurabh Burnwal <saurabhburnwal00@gmail.com>
2026-09-17 00:03:36 +0530

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.java
@@ -0,0 +1,40 @@
+class Solution {
+    
+    private int[] state;
+
+    public boolean validTree(int n, int[][] edges) {
+        if (edges.length != n - 1)  return false;
+        
+        state = new int[n];
+        
+        List<Integer>[] adj = new ArrayList[n];
+        Arrays.setAll(adj, i -> new ArrayList<>());
+
+        for (int[] edge : edges) {
+            adj[edge[0]].add(edge[1]);
+            adj[edge[1]].add(edge[0]);
+        }
+
+        if (hasCycle(adj, 0))   return false;
+        for (int s : state) if (s != 2) return false;
+
+        return true;
+    }
+
+    private boolean hasCycle(List<Integer>[] adj, int node) {
+        if (state[node] == 1)   return true;
+        if (state[node] == 2)   return false;
+
+        state[node] = 1;
+
+        for (int i : adj[node]) {
+            if (adj[i].contains(node)) {
+                adj[i].remove(Integer.valueOf(node));
+            }
+            if (hasCycle(adj, i))   return true;
+        }
+
+        state[node] = 2;
+        return false;
+    }
+}
