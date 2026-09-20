29ef34ebef67c20322eaaddff9c5e0aa487b35fb
Saurabh Burnwal <saurabhburnwal00@gmail.com>
2026-09-20 17:31:25 +0530

    Add: design-twitter-feed - submission-14

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-14.java
@@ -0,0 +1,38 @@
+class Twitter {
+
+    List<int[]> globalTweets;
+    Map<Integer, Set<Integer>> map;
+
+    public Twitter() {
+        globalTweets = new ArrayList<>();
+        map = new HashMap<>();
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        globalTweets.add(new int[] {userId, tweetId});
+        map.putIfAbsent(userId, new HashSet<>());
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> following = map.get(userId);
+        for (int i = globalTweets.size() - 1; i >= 0; i--) {
+            if (result.size() >= 10)    break;
+
+            int[] u = globalTweets.get(i);
+            if (u[0] == userId || (following != null && following.contains(u[0]))) {
+                result.add(u[1]);
+            }
+        }
+        return result;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        map.computeIfAbsent(followerId, k -> new HashSet<>()).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        Set<Integer> set = map.get(followerId);
+        if (set != null)    set.remove(followeeId);
+    }
+}
