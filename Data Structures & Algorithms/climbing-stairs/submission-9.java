e22d9bb4d4684224b89cb71cd1a10c9e2589cfd6
Saurabh Burnwal <saurabhburnwal00@gmail.com>
2026-09-22 12:32:05 +0530

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n < 3)  return n;
+        int one = 1;
+        int two = 2;
+        int res = 0;
+        for (int i = 3; i <= n; i++) {
+            res = one + two;
+            one = two;
+            two = res;
+        }
+
+        return res;
+    }
+}
